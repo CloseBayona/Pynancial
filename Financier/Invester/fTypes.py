c95159ca3ea6a15d20946f506b4c7358dd1dd172
Adrian <ronaldo13_96@outlook.com>
2022-05-20 19:31:08 -0500

    reorg

--- Financier/Invester/fTypes.py
+++ Financier/Invester/fTypes.py
@@ -1,3 +1,4 @@
 import typing
 
-sInterestRate = typing.NewType('Simple interest rate', float)
+sInterestRate = typing.NewType('Simple interest rate of return', float)
+lInterstRate = typing.NewType('Logarithmic interest rate of return', float)
--- Financier/Invester/interestRate.py
+++ Financier/Invester/interestRate.py
@@ -1,5 +1,19 @@
-from Financier.Invester.fTypes import sInterestRate
+from Financier.Invester.fTypes import sInterestRate, lInterstRate
+import numpy as np
 
-def sInterestRate(ending: float, beginning: float) -> sInterestRate:
+class interestRateOfReturn():
 
-    return (ending -  beginning) / beginning
+    def __init__(self, ending: float, beggining: float) -> None:
+        
+        self.ending = ending
+        self.beggining = beggining
+
+    def simple(self) -> sInterestRate:
+
+        return (self.ending -  self.beggining) / self.beggining
+
+    def logarithmic(self) -> lInterstRate:
+
+        interest = self.ending / self.beggining
+
+        return np.log(interest)
